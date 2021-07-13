7c57779d6cd0043af034cc1c19fc682e5ddc22c6
thebigdavec <daverbcollison@gmail.com>
2021-07-13 16:46:44 +0100

    Install and connect firebase

--- src/main.js
+++ src/main.js
@@ -6,4 +6,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import { initializeApp } from 'firebase/app'
+
+// Add the Firebase products that you want to use
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDz51KOsz_TkcQ-rq_h_jS34Dn5TZApBEk',
+  authDomain: 'pussst-afb48.firebaseapp.com',
+  projectId: 'pussst-afb48',
+  storageBucket: 'pussst-afb48.appspot.com',
+  messagingSenderId: '710451076574',
+  appId: '1:710451076574:web:e7490561097d69e1df514f',
+  measurementId: 'G-EVCGBNTPE9'
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
 createApp(App).use(router).use(store).mount('#app')
