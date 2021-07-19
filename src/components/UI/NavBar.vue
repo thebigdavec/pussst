af9a2af58a870899b53892aba133afaccaaf292c
thebigdavec <daverbcollison@gmail.com>
2021-07-19 16:39:55 +0100

    Update NavBar.vue
    Small bug fix in mobile nav list

--- src/components/UI/NavBar.vue
+++ src/components/UI/NavBar.vue
@@ -13,9 +13,9 @@
       >
         <button class="close-menu"></button>
         <li v-if="!isSignedIn">
-        <li v-if="!isSignedIn">
           <RouterLink :to="{ name: 'Sign In' }">Sign In</RouterLink>
         </li>
+        <li v-if="!isSignedIn">
           <RouterLink :to="{ name: 'Sign Up' }">Sign Up</RouterLink>
         </li>
         <li>
