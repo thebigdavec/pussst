f602585063f97c6e62d9596d050a52bc933309e1
thebigdavec <daverbcollison@gmail.com>
2022-06-28 19:21:16 +0100

    Minor Updates to Firebase and Vite

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,6 @@
 import TheHeader from './components/layout/TheHeader.vue'
 import TheAlert from './components/layout/TheAlert.vue'
 import TheFooter from './components/layout/TheFooter.vue'
-import Home from './views/Home.vue'
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
